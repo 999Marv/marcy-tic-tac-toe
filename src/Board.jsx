6a10cf202711883d9a0305e16c6d19d7fbb7965a
Marvin Siri <marvinsiri123@gmail.com>
2023-12-11 21:52:22 -0500

    board with squares set up

---
+++ src/Board.jsx
@@ -0,0 +1,23 @@
+import Square from './Square';
+
+export default function Board() {
+  return (
+    <>
+      <section>
+        <Square />
+        <Square />
+        <Square />
+      </section>
+      <section>
+        <Square />
+        <Square />
+        <Square />
+      </section>
+      <section>
+        <Square />
+        <Square />
+        <Square />
+      </section>
+    </>
+  );
+}
---
+++ src/Square.jsx
@@ -0,0 +1,3 @@
+export default function Square() {
+  return <button onClick={() => console.log('hi')}></button>;
+}
